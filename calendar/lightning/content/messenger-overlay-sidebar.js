ef695d33eda1a570994641cdc22b78e6d89ebf59
FranklinDM <mrmineshafter17@gmail.com>
2023-12-21 09:05:34 +0800

    Revert "No Issue - Lightning calendar integration isn't news on Epyrus."
    This reverts commit d116807adef916087cfb2766c4edfeb59896fe7f.
    
    Revert "No Issue - app.update.auto should be false by default."
    
    This reverts commit 50462aaea9c3be974a224b15e5ab1bf1bafc2fdc.
    
    Revert "Issue #77 - Make dialogheader use an orange gradient like the AppMenu."
    
    This reverts commit daf14ce1c05defe4310a320498ac2f3a5b2aa4a2.

--- calendar/lightning/content/messenger-overlay-sidebar.js
+++ calendar/lightning/content/messenger-overlay-sidebar.js
@@ -403,11 +403,8 @@ function ltnOnLoad(event) {
     };
 
     gCalendarUIPrefsObserver.onLoad();
-    // If we're on Epyrus, we don't need this... I think everyone knows
-    // what Lightning calendar is and whether they want it by now. 
-    if (Services.appinfo.ID != "{29877c1d-27df-4421-9a79-382c31470151}") {
-      ltnIntegrationCheck();
-    }
+
+    ltnIntegrationCheck();
 
     Services.obs.notifyObservers(window, "lightning-startup-done", false);
 }
